82742be42198406e0ea1726ec93cfa3520d019da
Jsakkos <jonathansakkos@protonmail.com>
2024-03-15 08:11:02 -0700

    Update config.py

--- mkv_episode_matcher/config.py
+++ mkv_episode_matcher/config.py
@@ -4,7 +4,7 @@ import configparser
 
 def set_config(api_key, show_dir, file):
     config = configparser.ConfigParser()
-    config["Config"] = {"api_key": api_key, "show_dir": show_dir}
+    config["Config"] = {"api_key": str(api_key), "show_dir": show_dir}
     with open(file, "w") as configfile:
         config.write(configfile)
 
@@ -12,5 +12,5 @@ def get_config(file):
     config = configparser.ConfigParser()
     if os.path.exists(file):
         config.read(file)
-        return dict(config.items("Config")) if "Config" in config else {}
+        return config["Config"] if "Config" in config else None
     return {}
