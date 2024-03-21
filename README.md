5e2fee0291598889647c7e4cb6590b14ba02f24a
Jsakkos <jonathansakkos@protonmail.com>
2024-03-20 17:24:05 -0700

    Update docs

--- README.md
+++ README.md
@@ -22,3 +22,25 @@ MKV Episode Matcher compares reference images from TMDb with frames from the mkv
 ## Caveats (WIP)
 
 Currently, MKV Episode Matcher is slow (several minutes per episode), CPU intensive, and error-prone.
+
+# Contributing
+
+Contributions are welcome! If you would like to contribute to the MKV Episode Matcher project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your contribution.
+3. Make your changes and commit them to your branch.
+4. Push your branch to your forked repository.
+5. Open a pull request to the main repository.
+
+Please ensure that your code follows the project's coding conventions and standards. Additionally, provide a clear and detailed description of your changes in the pull request.
+
+Thank you for your contribution!
+
+# License
+
+This project is licensed under the [Creative Commons Attribution-NonCommercial (CC BY-NC) license](https://creativecommons.org/licenses/by-nc/4.0/).
+
+# Acknowledgments
+This product uses the TMDB API but is not endorsed or certified by TMDB.
+![The Movie DB Logo](https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg)
--- mkv_episode_matcher/__main__.py
+++ mkv_episode_matcher/__main__.py
@@ -6,18 +6,49 @@ from .config import set_config, get_config
 from loguru import logger
 import sys
 
-logger.remove()  # Remove the default stdout handler
+# Remove the default stdout handler
+logger.remove()  
+
+# Check if logs directory exists, if not create it
 if not os.path.exists('./logs'):
     os.mkdir('./logs')
-logger.add("./logs/file_stdout.log", format="{time} {level} {message}", level="DEBUG", rotation="10 MB")  # Add a new handler for stdout logs
-logger.add("./logs/file_errors.log", level="ERROR", rotation="10 MB")  # Add a new handler for error logs
+
+# Add a new handler for stdout logs
+logger.add("./logs/file_stdout.log", format="{time} {level} {message}", level="DEBUG", rotation="10 MB")  
+
+# Add a new handler for error logs
+logger.add("./logs/file_errors.log", level="ERROR", rotation="10 MB")  
+
+# Check if the configuration directory exists, if not create it
 if not os.path.exists(os.path.join(os.path.expanduser("~"), ".mkv-episode-matcher")):
     os.makedirs(os.path.join(os.path.expanduser("~"), ".mkv-episode-matcher"))
+
+# Define the paths for the configuration file and cache directory
 CONFIG_FILE = os.path.join(os.path.expanduser("~"), ".mkv-episode-matcher","config.ini")
 CACHE_DIR = os.path.join(os.path.expanduser("~"), ".mkv-episode-matcher","cache")
+
+# Check if the cache directory exists, if not create it
 if not os.path.exists(CACHE_DIR):
     os.makedirs(CACHE_DIR)
+
 def main():
+    """
+    Entry point of the application.
+    
+    This function is responsible for starting the application, parsing command-line arguments,
+    setting the configuration, and processing the show.
+
+    Command-line arguments:
+    --api-key: The API key for the TMDb API. If not provided, the function will try to get it from the cache or prompt the user to input it.
+    --show-dir: The main directory of the show. If not provided, the function will prompt the user to input it.
+    --season: The season number to be processed. If not provided, all seasons will be processed.
+    --force: A boolean flag indicating whether to force rename files. If not provided, the function will not force rename files.
+    --dry-run: A boolean flag indicating whether to perform a dry run (i.e., not rename any files). If not provided, the function will rename files.
+    --threshold: The matching threshold for episode matching. If not provided, the function will use a default threshold.
+
+    The function logs its progress to two separate log files: one for standard output and one for errors.
+    """
+    # Log the start of the application
     logger.info("Starting the application")
 
     # Parse command-line arguments
@@ -53,12 +84,15 @@ def main():
         show_dir = input("Enter the main directory of the show: ")
     logger.debug(f"Show Directory: {show_dir}")
 
+    # Set the configuration
     set_config(api_key, show_dir, CONFIG_FILE)
     logger.info("Configuration set")
 
+    # Process the show
     from .episode_matcher import process_show
     process_show(args.season,force=args.force,dry_run=args.dry_run,threshold=args.threshold)
     logger.info("Show processing completed")
 
+# Run the main function if the script is run directly
 if __name__ == "__main__":
     main()
