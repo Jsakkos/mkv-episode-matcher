ae8794a3f8a8f43786c8f041c8b011a88e068b46
Jsakkos <jonathansakkos@protonmail.com>
2024-03-20 17:29:31 -0700

    Update docstrings

--- mkv_episode_matcher/tmdb_client.py
+++ mkv_episode_matcher/tmdb_client.py
@@ -13,35 +13,60 @@ import imagehash
 BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original"
 
 class RateLimitedRequest:
+    """
+    A class that represents a rate-limited request object.
+
+    Attributes:
+        rate_limit (int): Maximum number of requests allowed per period.
+        period (int): Period in seconds.
+        requests_made (int): Counter for requests made.
+        start_time (float): Start time of the current period.
+        lock (Lock): Lock for synchronization.
+    """
+
     def __init__(self, rate_limit=30, period=1):
-        self.rate_limit = rate_limit  # Maximum number of requests allowed per period
-        self.period = period  # Period in seconds
-        self.requests_made = 0  # Counter for requests made
-        self.start_time = time.time()  # Start time of the current period
-        self.lock = Lock()  # Lock for synchronization
+        self.rate_limit = rate_limit
+        self.period = period
+        self.requests_made = 0
+        self.start_time = time.time()
+        self.lock = Lock()
 
     def get(self, url):
+        """
+        Sends a rate-limited GET request to the specified URL.
+
+        Args:
+            url (str): The URL to send the request to.
+
+        Returns:
+            Response: The response object returned by the request.
+        """
         with self.lock:
-            # Check if the rate limit has been reached
             if self.requests_made >= self.rate_limit:
-                # Calculate the time to sleep until the start of the next period
                 sleep_time = self.period - (time.time() - self.start_time)
                 if sleep_time > 0:
                     time.sleep(sleep_time)
-                # Reset the counter and start time for the new period
                 self.requests_made = 0
                 self.start_time = time.time()
 
-            # Increment the counter
             self.requests_made += 1
 
-        # Make the request
         response = requests.get(url)
         return response
 
 # Initialize rate-limited request
 rate_limited_request = RateLimitedRequest(rate_limit=30, period=1)
 def calculate_image_hash_from_url(image_url):
+    """
+    Calculate the image hash from the given image URL.
+
+    Parameters:
+    image_url (str): The URL of the image.
+
+    Returns:
+    str: The calculated image hash as a string, or None if there was an error.
+
+    """
     try:
         response = rate_limited_request.get(image_url)
         if response.status_code == 200:
@@ -101,73 +126,22 @@ def fetch_season_details(show_id, season_number):
         logger.error(f"Missing 'episodes' key in response JSON data for Season {season_number}")
         return 0
     
-# def download_episode_images(show_id, season_number, episode_number, season_path):
-#     """
-#     Download images for a specific episode from the TMDb API.
-
-#     Args:
-#         show_id (str): The ID of the show on TMDb.
-#         season_number (int): The season number of the episode.
-#         episode_number (int): The episode number to download images for.
-#     """
-#     logger.info(f"Downloading images for Season {season_number} Episode {episode_number}...")
-#     episode_image_dir = os.path.join(season_path, f"downloaded_images/Season_{season_number}_Episode_{episode_number}")
-#     logger.info(f"Episode image directory: {episode_image_dir}")
-
-#     # Check if images already downloaded
-#     if os.path.exists(episode_image_dir) and len(os.listdir(episode_image_dir)) > 0:
-#         logger.info(f"Images for Season {season_number} Episode {episode_number} already downloaded.")
-#         return
-
-#     os.makedirs(episode_image_dir, exist_ok=True)
-
-#     # Construct the API URL for episode images
-#     config = get_config(CONFIG_FILE)
-#     api_key = config.get("api_key")
-#     url = f"https://api.themoviedb.org/3/tv/{show_id}/season/{season_number}/episode/{episode_number}/images?api_key={api_key}"
-#     logger.debug(f"API URL: {url}")
-#     try:
-#         logger.info(f"Fetching images for Season {season_number} Episode {episode_number}...")
-#         response = requests.get(url)
-#         response.raise_for_status()
-#         images = response.json().get('stills', [])
-#         for i, image in enumerate(images, start=1):
-#             image_url = BASE_IMAGE_URL + image['file_path']
-#             response = requests.get(image_url, stream=True)
-#             if response.status_code == 200:
-#                 image_path = os.path.join(episode_image_dir, f"image_{i}.jpg")
-#                 with open(image_path, 'wb') as file:
-#                     file.write(response.content)
-#                 logger.info(f"Downloaded {image_url} to {image_path}")
-#             else:
-#                 logger.error(f"Failed to download {image_url}")
-#     except requests.exceptions.RequestException as e:
-#         logger.error(f"Failed to fetch images for Season {season_number} Episode {episode_number}: {e}")
-
-# def download_season_images(show_id, season_number, season_path):
-#     """
-#     Download images for all episodes of a given season.
-
-#     Args:
-#         show_id (str): The ID of the show on TMDb.
-#         season_number (int): The season number to download images for.
-#         season_path (str): The path where the downloaded images will be saved.
-#     """
-#     logger.info(f"Downloading images for Season {season_number}...")
-#     season_dir = os.path.join(season_path, f"downloaded_images/Season_{season_number}")
-
-#     # Check if season images already downloaded
-#     if os.path.exists(season_dir) and len(os.listdir(season_dir)) > 0:
-#         logger.info(f"Images for Season {season_number} already downloaded.")
-#         return
-
-#     total_episodes = fetch_season_details(show_id, season_number)
-
-#     with ThreadPoolExecutor() as executor:
-#         for episode_number in range(1, total_episodes + 1):
-#             executor.submit(download_episode_images, show_id, season_number, episode_number, season_path)
-#     return total_episodes
 def fetch_and_hash_episode_images(show_id, season_number, episode_number):
+    """
+    Fetches and hashes the images for a specific episode of a TV show.
+
+    Args:
+        show_id (int): The ID of the TV show.
+        season_number (int): The season number of the episode.
+        episode_number (int): The episode number.
+
+    Returns:
+        list: A list of hash values for the episode images.
+
+    Raises:
+        requests.exceptions.RequestException: If there is an error while fetching the images.
+
+    """
     logger.info(f"Fetching and hashing images for Season {season_number} Episode {episode_number}...")
 
     # Construct the API URL for episode images
@@ -194,7 +168,17 @@ def fetch_and_hash_episode_images(show_id, season_number, episode_number):
         logger.error(f"Failed to fetch images for Season {season_number} Episode {episode_number}: {e}")
         return None
 
-def fetch_and_hash_season_images(show_id, season_number):
+def fetch_and_hash_season_images(show_id: int, season_number: int) -> None:
+    """
+    Fetches and hashes the images for a given season of a TV show.
+
+    Args:
+        show_id (int): The ID of the TV show.
+        season_number (int): The season number.
+
+    Returns:
+        dict: A dictionary containing the episode numbers as keys and their corresponding image hashes as values.
+    """
     logger.info(f"Fetching images for Season {season_number}...")
 
     total_episodes = fetch_season_details(show_id, season_number)
